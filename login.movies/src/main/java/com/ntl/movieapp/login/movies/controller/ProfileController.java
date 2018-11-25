d0f8ccdd01c923d485777feab9b0f43bd2781a4a
nalin <nagrawal327@gmail.com>
2018-11-26 02:45:45 +0530

    backend first

---
+++ login.movies/src/main/java/com/ntl/movieapp/login/movies/controller/ProfileController.java
@@ -0,0 +1,36 @@
+package com.ntl.movieapp.login.movies.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ntl.movieapp.login.movies.model.ProfileBean;
+import com.ntl.movieapp.login.movies.service.ProfileService;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
+public class ProfileController {
+	
+	@Autowired
+	ProfileService profileService;
+
+	public ProfileController() {
+		super();
+	}
+	
+	public ProfileController(ProfileService profileService) {
+		this.profileService=profileService;
+	}
+	
+	
+	@PostMapping("/register")
+	public ProfileBean register(@RequestBody ProfileBean profileBean) {
+		System.out.println("Rashiiiiiiiiiiiii");
+		return profileService.register(profileBean);
+	}
+	
+	
+
+}
